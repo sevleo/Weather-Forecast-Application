f4b88995cdec38bdf16b950918495a66042f0f05
Seva <seva.leonov@hotmail.com>
2024-07-10 13:52:51 +0700

    Add weather forecast get

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,10 @@ interface Location {
   country: string
   latitude: number
   longitude: number
+  weather?: {
+    forecast: string
+    currentConditions: string
+  }
 }
 
 const cityName = ref<string>('Berlin')
@@ -26,6 +30,32 @@ const fetchLocations = async () => {
       }
     })
     locations.value = response.data.results
+
+    await Promise.all(
+      locations.value.map(async (location) => {
+        try {
+          const weatherResponse = await axios.get('https://api.open-meteo.com/v1/forecast', {
+            params: {
+              latitude: location.latitude,
+              longitude: location.longitude,
+              hourly: 'temperature_2m',
+              start_date: '2024-04-02',
+              end_date: '2024-04-03'
+            }
+          })
+          location.weather = {
+            forecast: weatherResponse.data.forecast,
+            currentConditions: weatherResponse.data.currentConditions
+          }
+        } catch (err) {
+          console.error('Failed to fetch weather data for location:', location.name, err)
+          location.weather = {
+            forecast: 'N/A',
+            currentConditions: 'N/A'
+          }
+        }
+      })
+    )
   } catch (err) {
     error.value =
       axios.isAxiosError(err) && err.response ? err.response.data : 'An unknown error occurred'
@@ -57,8 +87,12 @@ onMounted(fetchLocations)
     <div v-if="error">{{ error }}</div>
     <ul v-else>
       <li v-for="location in locations" :key="location.latitude + location.longitude">
-        {{ location.name }}, {{ location.country }} (Lat: {{ location.latitude }}, Lon:
-        {{ location.longitude }} )
+        <h3>{{ location.name }}, {{ location.country }}</h3>
+        <p>Forecast: {{ location.weather ? location.weather.forecast : 'Loading...' }}</p>
+        <p>
+          Current Conditions:
+          {{ location.weather ? location.weather.currentConditions : 'Loading...' }}
+        </p>
       </li>
     </ul>
   </main>
